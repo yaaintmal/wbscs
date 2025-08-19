348e5b5e0eddff8ca6d324c86369af2af4662e4d
yaaintmal <me@problack.de>
2025-08-19 15:36:42 +0200

    some comparison with numbers, strings and operators

---
+++ JS/l3/topic11.js
@@ -0,0 +1,30 @@
+let start = 0;
+
+let val1 = 5;
+let val2 = "5";
+let val3 = 10;
+let val4 = "10";
+let val5 = true;
+let val6 = false;
+
+console.log(val1 == val2); // true
+console.log(val1 === val2); // false
+
+console.log(val3 == val4); // true
+console.log(val3 === val4); // false
+
+console.log(val1 > 3); // true
+console.log(val2 > 3); // true
+
+console.log(val3 < 20); // true
+console.log(val4 < 20); // true
+
+console.log(val1 >= 5); // true
+console.log(val1 >= 5); // true
+
+console.log(val3 <= 20); // true
+console.log(val4 <= 20); // true
+
+// just some more texts
+console.log(val5 == val6); // false
+console.log(val5 !== val6); // true
