038293e95e7e174e8e130a833a1313e4dbcbd0b5
yaaintmal <me@problack.de>
2025-08-19 14:34:00 +0200

    Starting lesson3 Topic 4 of JS

---
+++ JS/l3/topic4.js
@@ -0,0 +1,18 @@
+let age = 25;
+const birthYear = 1999;
+let name = "John Doe";
+const isStudent = true;
+
+age = 26;
+name = "Jane Doe";
+
+console.log(`Your Name is ${name} & your ${age} yrs old `);
+
+let favoriteColor = "blue";
+console.log(`Your favorite color is ${favoriteColor}`);
+favoriteColor = "green";
+if (favoriteColor === "blue") {
+  console.log(`Your favorite color is still ${favoriteColor}`);
+} else {
+  console.log(`Your favorite color is now ${favoriteColor}`);
+}
