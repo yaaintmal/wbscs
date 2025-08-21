d77edd1ba5b86c7c3b4de31a0826fb8c570d41c6
yaaintmal <me@problack.de>
2025-08-21 11:36:36 +0200

    reverse & toReversed

---
+++ JS/l5/l5t3.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log("original array: ", numbers);
+const numbersReversed = numbers.reverse();
+console.log(numbersReversed); // [5, 4, 3, 2, 1]
+
+const numbersSorted = numbers.sort();
+console.log(numbersSorted); // [1, 2, 3, 4, 5]
+
+const numbersToReverse = numbers.toReversed();
+console.log(numbersToReverse); // [5, 4, 3, 2, 1]
