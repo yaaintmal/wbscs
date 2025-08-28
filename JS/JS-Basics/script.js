0c6d05037e3a4ac952666c32bef1cb468e36336e
yaaintmal <me@problack.de>
2025-08-28 10:57:37 +0200

    tutoring session

--- JS/JS-Basics/script.js
+++ JS/JS-Basics/script.js
@@ -2,4 +2,22 @@ let start = 0;
 
 console.log("a hello from mal");
 
-document.querySelector("h1").textContent = "a hello from mal again";
+// document.querySelector("h1").textContent = "a hello from mal again";
+// console.log(5 + "5");
+
+const age = 27;
+const minimumAge = 18;
+
+if (age >= minimumAge) {
+  console.log("You are old enough to drive");
+}
+
+// logical short circuting
+// wird nur ausgeführt wenn die erste Bedingung wahr ist
+42 === 42 && console.log("42");
+// wird nur ausgeführt wenn die erste Bedingung falsch ist
+43 !== 43 && console.log("43");
+
+if (age >= minimumAge && age < 65) {
+  console.log("You are old enough to drive but not old enough to retire");
+}
