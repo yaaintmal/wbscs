edb94a3d23c0ceae596f3a0ac12398c69671098a
yaaintmal <me@problack.de>
2025-09-15 11:59:27 +0200

    refactored

--- JS/l13/src/main.js
+++ JS/l13/src/main.js
@@ -9,13 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch(API_URL) //
     .then((response) => response.json())
     .then((data) => {
-      // Take the first 9 items from the data
+      // took the first 9 items from the data
       const products = data.slice(0, 9);
       renderProducts(products, productGrid);
     })
     .catch((error) => {
       console.error("Error fetching products:", error);
-      // The error object itself is not a string, so we'll use a generic message.
+      // The error object itself is not a string, so we'll use a generic message << defined at top
       renderError(errorMessage, productGrid);
     });
 });
